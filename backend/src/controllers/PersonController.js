205727245cd557144a3d456508d087dcdafc114e
Samuel <samuelferreirarezende@gmail.com>
2021-05-31 20:18:11 -0300

    feat: added backend

---
+++ backend/src/controllers/PersonController.js
@@ -0,0 +1,75 @@
+const axios = require('axios')
+const Person = require('../model/Person')
+
+
+module.exports = {
+    async index(req, res){
+        const people = await Person.find({}).sort({RazaoSocial: 1})
+
+        return res.json(people)
+
+    },
+
+    async store(req, res){
+        const { RazaoSocial, Telefone1, Telefone1Contato, Telefone2, Telefone2Contato, Telefone3, Telefone3Contato, Telefone4, Telefone4Contato, Telefone5, Telefone5Contato, Email, Endereco, Observacoes } = req.body
+
+        const addPerson = await Person.create({
+            RazaoSocial: RazaoSocial,
+            Telefone1: Telefone1,
+            Telefone1Contato: Telefone1Contato,
+            Telefone2:  Telefone2,
+            Telefone2Contato: Telefone2Contato,
+            Telefone3: Telefone3,
+            Telefone3Contato: Telefone3Contato,
+            Telefone4: Telefone4,
+            Telefone4Contato: Telefone4Contato,
+            Telefone5: Telefone5,
+            Telefone5Contato: Telefone5Contato,
+            Email: Email,
+            Endereco: Endereco,
+            Observacoes: Observacoes
+        })
+
+        return res.json(addPerson)
+    },
+
+    async show(req, res){
+        const { id } = req.body
+
+        const showPerson = await Person.findById(id)
+
+        return res.json(showPerson)
+
+    },
+
+    async update(req, res){
+        const { id, RazaoSocial, Telefone1, Telefone1Contato, Telefone2, Telefone2Contato, Telefone3, Telefone3Contato, Telefone4, Telefone4Contato, Telefone5, Telefone5Contato, Email, Endereco, Observacoes } = req.body
+
+        const editPerson = await Person.findByIdAndUpdate(id, {
+            RazaoSocial: RazaoSocial,
+            Telefone1: Telefone1,
+            Telefone1Contato: Telefone1Contato,
+            Telefone2: Telefone2,
+            Telefone2Contato: Telefone2Contato,
+            Telefone3: Telefone3,
+            Telefone3Contato: Telefone3Contato,
+            Telefone4: Telefone4,
+            Telefone4Contato: Telefone4Contato,
+            Telefone5: Telefone5,
+            Telefone5Contato: Telefone5Contato,
+            Email: Email,
+            Endereco: Endereco,
+            Observacoes: Observacoes
+        })
+
+        return res.json(editPerson)
+    },
+
+    async delete(req, res){
+        const { id } = req.body 
+
+        const deletePerson = await Person.findByIdAndDelete(id)
+
+        return res.json(deletePerson)
+    }
+}
---
+++ backend/src/routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const routes = express.Router()
+const PersonController = require('./controllers/PersonController')
+
+routes.get('/', PersonController.index)
+routes.post('/add', PersonController.store)
+routes.get('/show', PersonController.show)
+routes.post('/update', PersonController.update)
+routes.post('/delete', PersonController.delete)
+
+module.exports = routes
