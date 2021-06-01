241de122dd0a41635d3e4c1435ccaed84665455e
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2021-06-01 16:04:13 -0300

    feat: added frontend-edit page

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,7 @@ const PersonController = require('./controllers/PersonController')
 
 routes.get('/', PersonController.index)
 routes.post('/add', PersonController.store)
-routes.get('/show', PersonController.show)
+routes.post('/show', PersonController.show)
 routes.post('/update', PersonController.update)
 routes.post('/delete', PersonController.delete)
 
--- frontend/src/pages/Edit.js
+++ frontend/src/pages/Edit.js
@@ -1,28 +1,44 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/Header'
+
 
 import api from '../services/api'
 
-export default function Edit({ history }){
-    const [RazaoSocial, setRazaoSocial] = useState('')
-    const [Endereco, setEndereco] = useState('')
-    const [Email, setEmail] = useState('')
-    const [Telefone1, setTelefone1] = useState('')
-    const [Telefone1Contato, setTelefone1Contato] = useState('')
-    const [Telefone2, setTelefone2] = useState('')
-    const [Telefone2Contato, setTelefone2Contato] = useState('')
-    const [Telefone3, setTelefone3] = useState('')
-    const [Telefone3Contato, setTelefone3Contato] = useState('')
-    const [Telefone4, setTelefone4] = useState('')
-    const [Telefone4Contato, setTelefone4Contato] = useState('')
-    const [Telefone5, setTelefone5] = useState('')
-    const [Telefone5Contato, setTelefone5Contato] = useState('')
-    const [Observacoes, setObservacoes] = useState('')
+export default function Edit({ match, history }){
+    const [person, setPerson] = useState('')
+    useEffect(() => {
+        async function loadPerson(){
+            const id = match.params.id
+            const response = await api.post('/show', {
+                id
+            })
+            setPerson(response.data)
+        }
+        loadPerson()
+        
+    }, [match.params.id])
+
+
+    const [RazaoSocial, setRazaoSocial] = useState()
+    const [Endereco, setEndereco] = useState()
+    const [Email, setEmail] = useState()
+    const [Telefone1, setTelefone1] = useState()
+    const [Telefone1Contato, setTelefone1Contato] = useState()
+    const [Telefone2, setTelefone2] = useState()
+    const [Telefone2Contato, setTelefone2Contato] = useState()
+    const [Telefone3, setTelefone3] = useState()
+    const [Telefone3Contato, setTelefone3Contato] = useState()
+    const [Telefone4, setTelefone4] = useState()
+    const [Telefone4Contato, setTelefone4Contato] = useState()
+    const [Telefone5, setTelefone5] = useState()
+    const [Telefone5Contato, setTelefone5Contato] = useState()
+    const [Observacoes, setObservacoes] = useState()
 
     async function handleSubmit(e){
         e.preventDefault()
-
-        await api.post('/edit',{
+        const id = match.params.id
+        const response = await api.post('/update', {
+            id,
             RazaoSocial,
             Endereco,
             Email,
@@ -39,49 +55,39 @@ export default function Edit({ history }){
             Observacoes
         })
 
+        console.log(response)
+
         history.push('/')
     }
 
+    
+
     return (
         <div>
-           <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
     rel="stylesheet" />
+        <Header />
+
+            <form onSubmit={handleSubmit}>
+            <input type="text" defaultValue={person.RazaoSocial} onChange={e => setRazaoSocial(e.target.value)} />
+            <input type="text" defaultValue={person.Telefone1} onChange={e => setTelefone1(e.target.value)} />
+            <input type="text" defaultValue={person.Telefone1Contato} onChange={e => setTelefone1Contato(e.target.value)} />
+            <input type="text" defaultValue={person.Telefone2} onChange={e => setTelefone2(e.target.value)} />
+            <input type="text" defaultValue={person.Telefone2Contato} onChange={e => setTelefone2Contato(e.target.value)} />
+            <input type="text" defaultValue={person.Telefone3} onChange={e => setTelefone3(e.target.value)} />
+            <input type="text" defaultValue={person.Telefone3Contato} onChange={e => setTelefone3Contato(e.target.value)} />
+            <input type="text" defaultValue={person.Telefone4} onChange={e => setTelefone4(e.target.value)} />
+            <input type="text" defaultValue={person.Telefone4Contato} onChange={e => setTelefone4Contato(e.target.value)} />
+            <input type="text" defaultValue={person.Telefone5} onChange={e => setTelefone5(e.target.value)} />
+            <input type="text" defaultValue={person.Telefone5Contato} onChange={e => setTelefone5Contato(e.target.value)} />
+            <input type="text" defaultValue={person.Email} onChange={e => setEmail(e.target.value)} />
+            <input type="text" defaultValue={person.Endereco} onChange={e => setEndereco(e.target.value)} />
+            <input type="text" defaultValue={person.Observacoes} onChange={e => setObservacoes(e.target.value)} />
+            <button type="submit">ENVIAR</button>
+            </form>
+
             
-            <div>
-            <Header />     
-
-            <div className="container w-50">
-                <h1 className="text-black text-center mt-5 mb-3">Editar Contato</h1>
-                    <form onSubmit={handleSubmit} className="d-flex flex-column">
-                        <input className="form-control mb-2" placeholder="Nome" value={RazaoSocial} onChange={e => setRazaoSocial(e.target.value)} />
-                        <input className="form-control mb-2" placeholder="Endereço" value={Endereco} onChange={e => setEndereco(e.target.value)} />
-                        <input className="form-control mb-2" placeholder="Email" value={Email} onChange={e => setEmail(e.target.value)} />
-                        <div className="d-inline">
-                            <input className="form-control mb-2" placeholder="Telefone 1" value={Telefone1} onChange={e => setTelefone1(e.target.value)} />
-                            <input className="form-control mb-2 ml-5" placeholder="Contato 1" value={Telefone1Contato} onChange={e => setTelefone1Contato(e.target.value)} />
-                        </div>
-                        <div className="form-inline">
-                            <input className="form-control mb-2" placeholder="Telefone 2" value={Telefone2} onChange={e => setTelefone2(e.target.value)} />
-                            <input className="form-control mb-2 ml-5" placeholder="Contato 2" value={Telefone2Contato} onChange={e => setTelefone2Contato(e.target.value)} />
-                        </div>
-                        <div className="form-inline">
-                            <input className="form-control mb-2" placeholder="Telefone 3" value={Telefone3} onChange={e => setTelefone3(e.target.value)} />
-                            <input className="form-control mb-2 ml-5" placeholder="Contato 3" value={Telefone3Contato} onChange={e => setTelefone3Contato(e.target.value)} />
-                        </div>
-                        <div className="form-inline">
-                            <input className="form-control mb-2" placeholder="Telefone 4" value={Telefone4} onChange={e => setTelefone4(e.target.value)} />
-                            <input className="form-control mb-2 ml-5" placeholder="Contato 4" value={Telefone4Contato} onChange={e => setTelefone4Contato(e.target.value)} />
-                        </div>
-                        <div className="form-inline">
-                            <input className="form-control mb-2" placeholder="Telefone 5" value={Telefone5} onChange={e => setTelefone5(e.target.value)} />
-                            <input className="form-control mb-2 ml-5" placeholder="Contato 5" value={Telefone5Contato} onChange={e => setTelefone5Contato(e.target.value)} />
-                        </div>
-
-                        <input className="form-control mb-4" placeholder="Observações" value={Observacoes} onChange={e => setObservacoes(e.target.value)} />
-                        <button type="submit" className="btn btn-primary">Editar</button>
-                    </form>
-            </div>
-            </div>
         </div>
     )
 }
