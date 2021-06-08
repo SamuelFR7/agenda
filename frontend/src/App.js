4dc53ae21570a8d1d8e86e0e099d188c0cdd5a50
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2021-06-08 14:26:15 -0300

    feat: added styles in main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './App.css'
 import Routes from './routes'
 
-export default function App() {
+function App() {
   return (
     <Routes />
   );
 }
+
+export default App
 
