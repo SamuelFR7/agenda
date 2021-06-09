83688804d4f95d5a55019a61a6b9905e490db496
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2021-06-09 14:54:47 -0300

    feat: added jwt authentication

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,14 +5,16 @@ import Main from './pages/Main'
 import Add from './pages/Add'
 import Edit from './pages/Edit'
 import Show from './pages/Show'
+import Login from './pages/Login'
 
 export default function Routes(){
     return (
         <BrowserRouter>
-            <Route path="/" exact component={Main} />
+            <Route path="/main" component={Main} />
             <Route path="/add" component={Add} />
             <Route path="/edit/:id" component={Edit} />
             <Route path="/show/:id" component={Show}/>
+            <Route path="/" exact component={Login} />
         </BrowserRouter>
     )
 }
