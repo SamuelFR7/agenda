7b5fe0ff33e154fa6072cbd4e06a7ce9eb7251d9
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2021-06-23 14:22:45 -0300

    style: added error login modal

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -5,11 +5,13 @@ import api from '../services/api'
 
 
 import logo from '../assets/logo.svg'
+import cancel from '../assets/cancel.png'
 
 export default function Login({ history }){
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [logged, setLogged] = useState(false)
+    const [error, setError] = useState(false)
 
     useEffect(() => {
         async function Check(){
@@ -31,23 +33,30 @@ export default function Login({ history }){
 
     async function handleSubmit(e){
         e.preventDefault()
-        const response = await api.get('/user/login', {
-            headers: {
-                email: email,
-                password: password
-            }
-        })
-        const datares = response.data
-        const token = datares.token
-        localStorage.setItem('token', token)
-        history.push('/main')
+        try {
+            const response = await api.get('/user/login', {
+                headers: {
+                    email: email,
+                    password: password
+                }
+            })
+            const datares = response.data
+            const token = datares.token
+            localStorage.setItem('token', token)
+            history.push('/main')
+        } catch (error) {
+            setError(true)
+        }
+        
     }
 
     if (logged){
         history.push('/main')
     }
 
-    
+    if (error){
+        console.log("Erou")
+    }
 
     return (
         <div className="login-container">
@@ -66,6 +75,14 @@ export default function Login({ history }){
                 onChange={e => setPassword(e.target.value)}
                 />
                 <button type="submit">Entrar</button>
+                {error && (
+                <div className="error-container">
+                    <div className="error-content">
+                        <p>Usuário ou senha incorretos</p>
+                        <div onClick={() => setError(false)}><img src={cancel} alt="Cancel"></img></div>
+                    </div>
+                </div>
+            )}
             </form>
         </div>
     )
