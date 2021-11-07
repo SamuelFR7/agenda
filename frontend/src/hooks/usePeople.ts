a46bf3ba4ef58a0246760def694de287f5d1435c
Samuel Rezende <samuelferreirarezende@gmail.com>
2021-11-07 17:45:54 -0300

    feat: new layout completed

---
+++ frontend/src/hooks/usePeople.ts
@@ -0,0 +1,8 @@
+import { useContext } from 'react'
+import { PeopleContext } from '../contexts/PeopleContext'
+
+export function usePeople () {
+  const context = useContext(PeopleContext)
+
+  return context
+}
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,13 +3,16 @@ import type { AppProps } from 'next/app'
 
 import { GlobalStyle } from '../styles/Global'
 import { AuthProvider } from '../contexts/AuthContext'
+import { PeopleProvider } from '../contexts/PeopleContext'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
       <>
         <AuthProvider>
-          <Component {...pageProps} />
-          <GlobalStyle />
+          <PeopleProvider>
+            <Component {...pageProps} />
+            <GlobalStyle />
+          </PeopleProvider>
         </AuthProvider>
       </>
   )
