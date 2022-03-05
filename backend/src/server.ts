939562a513753f2d17cdcd0947a9d21703959dac
Samuel Rezende <samuelferreirarezende@gmail.com>
2022-03-05 00:27:41 -0300

    fix: express limit json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,14 +11,10 @@ dotenv.config()
 const app = express()
 
 app.use(cors())
-app.use(
-    express.json({
-        limit: '50mb',
-    })
-)
+app.use(express.json())
 app.use(router)
 app.use(errorCatch)
 
 app.listen(process.env.PORT || 3333, () =>
-    console.log('🚀 Server started and running at port 3333')
+    console.log('🚀 Server started and running at port 3333!')
 )
