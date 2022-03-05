fc736682c6c427b4fde6d9e1c2a30e54589c1ab7
Samuel Rezende <samuelferreirarezende@gmail.com>
2022-03-05 16:47:48 -0300

    fix: babel config

--- backend/babel.config.js
+++ backend/babel.config.js
@@ -1,17 +1,20 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current'
-        }
-      }
+    presets: [
+        ['@babel/preset-env', { targets: { node: 'current' }}],
+        '@babel/preset-typescript',
     ],
-    '@babel/preset-typescript'
-  ],
-  plugins: [],
-  ignore: [
-    '**/*.spec.ts'
-  ]
+    plugins: [
+        [
+        'module-resolver',
+        {
+            alias: {
+                "@modules": "./src/modules",
+                "@shared": "./src/shared"
+            }
+        }
+    ],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+],
 }
