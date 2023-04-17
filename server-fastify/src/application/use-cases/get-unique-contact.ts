0900c048a48e137b89479b459251b7db4ed4c8e9
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-04-17 14:46:07 -0300

    feat: get unique contact use case

---
+++ server-fastify/src/application/use-cases/get-unique-contact.ts
@@ -0,0 +1,26 @@
+import { Contact } from '@/domain/entities/Contact'
+import { ContactsRepository } from '@/domain/repositories/contacts-repository'
+
+interface GetUniqueContactRequest {
+  id: string
+}
+
+interface GetUniqueContactResponse {
+  contact: Contact
+}
+
+export class GetUniqueContact {
+  constructor(private readonly contactsRepository: ContactsRepository) {}
+
+  async execute(
+    req: GetUniqueContactRequest,
+  ): Promise<GetUniqueContactResponse> {
+    const { id } = req
+
+    const contact = await this.contactsRepository.findUnique(id)
+
+    return {
+      contact,
+    }
+  }
+}
