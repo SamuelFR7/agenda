8a3e4483796e698243752d5008d4126060615534
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-04-17 15:59:25 -0300

    feat: users routes

---
+++ server-fastify/src/infra/app.ts
@@ -0,0 +1,32 @@
+import { env } from '@/config/env'
+import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
+import fastify from 'fastify'
+import { usersRoutes } from './http/routes/users-routes'
+import { ZodError } from 'zod'
+
+export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
+app.register(fastifyCors, {})
+
+app.register(usersRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
