5ecc9c3e6ad0f2e544adbf0596070e423e44596e
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-04-17 15:34:35 -0300

    feat: create contact controller

---
+++ server-fastify/src/infra/http/controllers/create-contact.ts
@@ -0,0 +1,66 @@
+import { CreateContactUseCase } from '@/application/use-cases/create-contact'
+import { PrismaContactsRepository } from '@/infra/database/prisma/repositories/prisma-contacts-repository'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export class CreateContactController {
+  async handle(req: FastifyRequest, res: FastifyReply) {
+    const createContactSchema = z.object({
+      name: z.string().nonempty({ message: 'Name should not be empty' }),
+      phone_1: z.string().nonempty({ message: 'Phone 1 should not be empty' }),
+      phone_2: z.string(),
+      phone_3: z.string(),
+      phone_4: z.string(),
+      phone_5: z.string(),
+      contact_1: z.string(),
+      contact_2: z.string(),
+      contact_3: z.string(),
+      contact_4: z.string(),
+      contact_5: z.string(),
+      email: z.string().email(),
+      address: z.string(),
+      observations: z.string(),
+    })
+
+    const {
+      name,
+      phone_1,
+      address,
+      contact_1,
+      contact_2,
+      contact_3,
+      contact_4,
+      contact_5,
+      email,
+      observations,
+      phone_2,
+      phone_3,
+      phone_4,
+      phone_5,
+    } = createContactSchema.parse(req.body)
+
+    const prismaContactsRepository = new PrismaContactsRepository()
+    const createContactUseCase = new CreateContactUseCase(
+      prismaContactsRepository,
+    )
+
+    await createContactUseCase.execute({
+      address,
+      contact_1,
+      contact_2,
+      contact_3,
+      contact_4,
+      contact_5,
+      email,
+      name,
+      observations,
+      phone_1,
+      phone_2,
+      phone_3,
+      phone_4,
+      phone_5,
+    })
+
+    return res.status(201).send()
+  }
+}
