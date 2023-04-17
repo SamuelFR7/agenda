4e0e137daec0cad748f05c06ad701ea06e176b00
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-04-17 14:52:06 -0300

    feat: get many contacts paginated

--- server-fastify/src/domain/repositories/contacts-repository.ts
+++ server-fastify/src/domain/repositories/contacts-repository.ts
@@ -2,8 +2,16 @@ import { Contact } from '../entities/Contact'
 
 export abstract class ContactsRepository {
   abstract deleteById(id: string): Promise<void>
-  abstract findMany(page?: number, search?: string): Promise<Contact[]>
+  abstract findMany({
+    page,
+    search,
+  }: {
+    page?: number
+    search?: string
+  }): Promise<Contact[]>
+
   abstract findUnique(id: string): Promise<Contact>
   abstract save(contact: Contact): Promise<void>
   abstract create(contact: Contact): Promise<void>
+  abstract count(): Promise<number>
 }
