97b8021928f6dc9f201f450b1eae23c955dfabd8
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-04-17 15:55:06 -0300

    feat: update contact controller

--- server-fastify/src/application/use-cases/update-contact.ts
+++ server-fastify/src/application/use-cases/update-contact.ts
@@ -2,18 +2,68 @@ import { Contact } from '@/domain/entities/Contact'
 import { ContactsRepository } from '@/domain/repositories/contacts-repository'
 
 interface UpdateContactRequest {
-  contactToUpdate: Contact
+  id: string
+  name: string
+  phone_1: string
+  phone_2: string
+  phone_3: string
+  phone_4: string
+  phone_5: string
+  contact_1: string
+  contact_2: string
+  contact_3: string
+  contact_4: string
+  contact_5: string
+  email: string
+  address: string
+  observations: string
 }
 
 interface UpdateContactResponse {
   contact: Contact
 }
 
-export class UpdateContact {
+export class UpdateContactUseCase {
   constructor(private readonly contactsRepository: ContactsRepository) {}
 
   async execute(req: UpdateContactRequest): Promise<UpdateContactResponse> {
-    const { contactToUpdate } = req
+    const {
+      id,
+      address,
+      contact_1,
+      contact_2,
+      contact_3,
+      contact_4,
+      contact_5,
+      email,
+      name,
+      observations,
+      phone_1,
+      phone_2,
+      phone_3,
+      phone_4,
+      phone_5,
+    } = req
+
+    const contactToUpdate = new Contact(
+      {
+        address,
+        contact_1,
+        contact_2,
+        contact_3,
+        contact_4,
+        contact_5,
+        email,
+        name,
+        observations,
+        phone_1,
+        phone_2,
+        phone_3,
+        phone_4,
+        phone_5,
+      },
+      id,
+    )
 
     const contact = await this.contactsRepository.save(contactToUpdate)
 
