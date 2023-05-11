c1b744afd19091f6f267c3758ee7157bc1e37dbd
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-05-11 09:44:33 -0300

    feat: initial config serverless

---
+++ serverless/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { type NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <main>
+        <h1>Agenda</h1>
+      </main>
+    </>
+  )
+}
+
+export default Home
