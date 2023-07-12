e6cec206b089b36311c76555a493249c3d3e8383
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-07-12 17:59:07 -0300

    feat: data table components

--- agenda-app-router/src/db/schema.ts
+++ agenda-app-router/src/db/schema.ts
@@ -2,18 +2,18 @@ import { type InferModel } from "drizzle-orm"
 import { mysqlTable, text, varchar } from "drizzle-orm/mysql-core"
 
 export const contacts = mysqlTable("contacts", {
-  id: varchar("id", { length: 191 }).primaryKey(),
+  id: varchar("id", { length: 191 }).primaryKey().notNull(),
   name: varchar("name", { length: 191 }).notNull(),
-  phone_1: varchar("phone_1", { length: 191 }).notNull(),
-  phone_2: varchar("phone_2", { length: 191 }),
-  phone_3: varchar("phone_3", { length: 191 }),
-  phone_4: varchar("phone_4", { length: 191 }),
-  phone_5: varchar("phone_5", { length: 191 }),
-  contact_1: varchar("contact_1", { length: 191 }),
-  contact_2: varchar("contact_2", { length: 191 }),
-  contact_3: varchar("contact_3", { length: 191 }),
-  contact_4: varchar("contact_4", { length: 191 }),
-  contact_5: varchar("contact_5", { length: 191 }),
+  phone1: varchar("phone_1", { length: 191 }).notNull(),
+  phone2: varchar("phone_2", { length: 191 }),
+  phone3: varchar("phone_3", { length: 191 }),
+  phone4: varchar("phone_4", { length: 191 }),
+  phone5: varchar("phone_5", { length: 191 }),
+  contact1: varchar("contact_1", { length: 191 }),
+  contact2: varchar("contact_2", { length: 191 }),
+  contact3: varchar("contact_3", { length: 191 }),
+  contact4: varchar("contact_4", { length: 191 }),
+  contact5: varchar("contact_5", { length: 191 }),
   email: varchar("email", { length: 191 }),
   address: varchar("address", { length: 191 }),
   observations: text("observations"),
@@ -22,7 +22,7 @@ export const contacts = mysqlTable("contacts", {
 export type Contact = InferModel<typeof contacts>
 
 export const users = mysqlTable("users", {
-  id: varchar("id", { length: 191 }).primaryKey(),
+  id: varchar("id", { length: 191 }).primaryKey().notNull(),
   username: varchar("username", { length: 191 }).notNull().unique(),
   password: varchar("password", { length: 191 }).notNull(),
 })
