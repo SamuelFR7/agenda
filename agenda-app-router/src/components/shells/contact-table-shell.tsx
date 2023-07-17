3b31e6bc2c5625211511ec8b2cbdaaf98ea9debb
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-07-17 09:13:43 -0300

    feat: debounced search

--- agenda-app-router/src/components/shells/contact-table-shell.tsx
+++ agenda-app-router/src/components/shells/contact-table-shell.tsx
@@ -1,8 +1,15 @@
+"use client"
+
+import React from "react"
+import { usePathname, useRouter } from "next/navigation"
 import { type Contact } from "@/db/schema"
 
+import { useDebounce } from "@/hooks/use-debounce"
+
+import { TableLoading } from "../table-loading"
 import { TablePagination } from "../table-pagination"
-import { TableToolbar } from "../table-toolbar"
 import { Card, CardContent } from "../ui/card"
+import { Input } from "../ui/input"
 import {
   Table,
   TableBody,
@@ -23,9 +30,29 @@ export function ContactTableShell({
   totalContacts,
   currentPage,
 }: ContactTableShellProps) {
+  const router = useRouter()
+  const pathname = usePathname()
+  const [isPending, startTransition] = React.useTransition()
+  const [query, setQuery] = React.useState("")
+  const debouncedQuery = useDebounce(query, 300)
+
+  React.useEffect(() => {
+    startTransition(() => {
+      router.push(`${pathname}?name=${debouncedQuery}`)
+    })
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedQuery])
+
   return (
     <>
-      <TableToolbar />
+      <div>
+        <Input
+          placeholder="Pesquisar pelo nome"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+      </div>
       <Card className="w-full">
         <CardContent className="p-0">
           <Table>
@@ -37,13 +64,17 @@ export function ContactTableShell({
               </TableRow>
             </TableHeader>
             <TableBody>
-              {allContacts.map((contact) => (
-                <TableRow key={contact.id}>
-                  <TableCell>{contact.name}</TableCell>
-                  <TableCell>{contact.phone1}</TableCell>
-                  <TableCell>{contact.email}</TableCell>
-                </TableRow>
-              ))}
+              {isPending ? (
+                <TableLoading />
+              ) : (
+                allContacts.map((contact) => (
+                  <TableRow key={contact.id}>
+                    <TableCell>{contact.name}</TableCell>
+                    <TableCell>{contact.phone1}</TableCell>
+                    <TableCell>{contact.email}</TableCell>
+                  </TableRow>
+                ))
+              )}
             </TableBody>
           </Table>
         </CardContent>
