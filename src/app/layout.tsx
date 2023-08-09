1044f3823d07b13d502ec7aa4877631d8ca3830f
Samuel Rezende <samuelferreirarezende@gmail.com>
2023-08-09 14:44:50 -0300

    feat: remove clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "@/styles/globals.css"
 
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import { ClerkProvider, currentUser } from "@clerk/nextjs"
 
 import { Header } from "@/components/header"
 
@@ -23,17 +22,14 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const user = await currentUser()
 
   return (
-    <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <Header user={user} />
+          <Header user={null} />
           <main>{children}</main>
         </body>
         <Toaster />
       </html>
-    </ClerkProvider>
   )
 }
