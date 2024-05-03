1c3012346d08525e46358a49143b6ef19e39f717
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2024-05-03 09:00:05 -0300

    feat: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "@/styles/globals.css"
 
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -35,6 +36,7 @@ export default function RootLayout({
             <main>{children}</main>
           </div>
         </ThemeProvider>
+        <Analytics />
       </body>
       <Toaster />
     </html>
